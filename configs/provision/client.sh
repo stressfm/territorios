03bd9a76febb0ef278a98c48cd024bac27a0a51d
vasco <vascopitaf@gmail.com>
2017-03-24 21:59:33 +0000

    fix small bug in provision for client and server

--- configs/provision/client.sh
+++ configs/provision/client.sh
@@ -2,35 +2,33 @@
 
 INSTALL_USER="vagrant"
 
-# echo "deb http://matriz.stress.fm/deb_repo jessie main" > /etc/apt/sources.list.d/matriz.list
-# wget -O - http://matriz.stress.fm/deb_repo/matriz_deb.gpg.asc | apt-key add -
-# 
-# apt-get update
-# apt-get dist-upgrade
-# apt-get install debconf-utils -y
-# debconf-set-selections <<< "jackd2    jackd/tweak_rt_limits   boolean true"
-# apt-get install -y jackd2 \
-#    moc libgstrtspserver-1.0 \
-#    python-gst-1.0 gstreamer1.0-plugins-bad libgstreamer-plugins-bad1.0 \
-#    python-dev libffi-dev libssl-dev curl
-# 
-# curl -LO https://bootstrap.pypa.io/get-pip.py
-# python get-pip.py
-# pip install supervisor
-# 
-# pip install matriz
-# 
-# apt-get install -y rsync
-# sudo -u $INSTALL_USER mkdir /home/$INSTALL_USER/territorios
-# 
+echo "deb http://matriz.stress.fm/deb_repo jessie main" > /etc/apt/sources.list.d/matriz.list
+wget -O - http://matriz.stress.fm/deb_repo/matriz_deb.gpg.asc | apt-key add -
+
+apt-get update
+apt-get dist-upgrade
+apt-get install debconf-utils -y
+debconf-set-selections <<< "jackd2    jackd/tweak_rt_limits   boolean true"
+apt-get install -y jackd2 \
+   moc libgstrtspserver-1.0 \
+   python-gst-1.0 gstreamer1.0-plugins-bad libgstreamer-plugins-bad1.0 \
+   python-dev libffi-dev libssl-dev curl
+
+curl -LO https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install supervisor
+
+pip install matriz
+
+apt-get install -y rsync
+sudo -u $INSTALL_USER mkdir /home/$INSTALL_USER/territorios
+
 cp /vagrant/configs/provision/client.d/{rc.local,supervisord.conf} /etc
-# sudo -u $INSTALL_USER cp /vagrant/configs/provision/client.d/client.json /home/$INSTALL_USER/client.json
-# sudo -u $INSTALL_USER cp /vagrant/configs/provision/common.d/_.jackdrc /home/$INSTALL_USER/.jackdrc
-# sudo -u $INSTALL_USER cp /vagrant/configs/provision/client.d/update_code.sh /home/$INSTALL_USER
-# sudo -u $INSTALL_USER cp /vagrant/scripts/start_jackd.sh /home/$INSTALL_USER
-# chmod +x /home/$INSTALL_USER/{update_code.sh,start_jackd.sh}
+sudo -u $INSTALL_USER cp /vagrant/configs/provision/client.d/client.json /home/$INSTALL_USER/client.json
+sudo -u $INSTALL_USER cp /vagrant/configs/provision/common.d/_.jackdrc /home/$INSTALL_USER/.jackdrc
+sudo -u $INSTALL_USER cp /vagrant/configs/provision/client.d/update_code.sh /home/$INSTALL_USER
+sudo -u $INSTALL_USER cp /vagrant/scripts/start_jackd.sh /home/$INSTALL_USER
+chmod +x /home/$INSTALL_USER/{update_code.sh,start_jackd.sh}
 
+rsync -rv /vagrant/{scripts,src} /home/$INSTALL_USER/territorios/
 /etc/rc.local
-sleep 3
-sudo -u $INSTALL_USER /home/$INSTALL_USER/update_code.sh
-
--- configs/provision/server.sh
+++ configs/provision/server.sh
@@ -2,36 +2,35 @@
 
 INSTALL_USER="vagrant"
 
-# echo "deb http://matriz.stress.fm/deb_repo jessie main" >  /etc/apt/sources.list.d/matriz.list
-# wget -O - http://matriz.stress.fm/deb_repo/matriz_deb.gpg.asc 2>/dev/null | apt-key add -
-# 
-# apt-get update
-# apt-get install debconf-utils -y
-# debconf-set-selections <<< "jackd2    jackd/tweak_rt_limits   boolean true"
-# apt-get install -y jackd2 \
-#    moc libgstrtspserver-1.0 \
-#    python-gst-1.0 gstreamer1.0-plugins-bad libgstreamer-plugins-bad1.0 \
-#    python-dev libffi-dev libssl-dev curl
-# 
-# curl -LO https://bootstrap.pypa.io/get-pip.py
-# python get-pip.py
-# pip install supervisor
-# 
-# pip install -r /vagrant/requirements/server.txt
-# pip install -r /vagrant/requirements/client.txt
-# 
-# 
-# apt-get install -y rsync
-# sudo -u $INSTALL_USER mkdir /home/$INSTALL_USER/territorios
-# 
-# # Copy files
-cp -r /vagrant/configs/provision/server.d/{rc.local,supervisord} /etc
-# sudo -u $INSTALL_USER cp /vagrant/configs/provision/common.d/_.jackdrc /home/$INSTALL_USER/.jackdrc
-# sudo -u $INSTALL_USER cp /vagrant/configs/provision/server.d/update_code.sh /home/$INSTALL_USER
-# sudo -u $INSTALL_USER cp /vagrant/scripts/start_jackd.sh /home/$INSTALL_USER
-# chmod +x /home/$INSTALL_USER/{update_code.sh,start_jackd.sh}
+wget -O - http://matriz.stress.fm/deb_repo/matriz_deb.gpg.asc 2>/dev/null | apt-key add -
+echo "deb http://matriz.stress.fm/deb_repo jessie main" >  /etc/apt/sources.list.d/matriz.list
 
+apt-get update
+apt-get install debconf-utils -y
+debconf-set-selections <<< "jackd2    jackd/tweak_rt_limits   boolean true"
+apt-get install -y jackd2 \
+   moc libgstrtspserver-1.0 \
+   python-gst-1.0 gstreamer1.0-plugins-bad libgstreamer-plugins-bad1.0 \
+   python-dev libffi-dev libssl-dev curl
+
+curl -LO https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install supervisor
+
+pip install -r /vagrant/requirements/server.txt
+pip install -r /vagrant/requirements/client.txt
+
+
+apt-get install -y rsync
+sudo -u $INSTALL_USER mkdir /home/$INSTALL_USER/territorios
+
+## Copy files
+cp -r /vagrant/configs/provision/server.d/{rc.local,supervisord} /etc
+sudo -u $INSTALL_USER cp /vagrant/configs/provision/common.d/_.jackdrc /home/$INSTALL_USER/.jackdrc
+sudo -u $INSTALL_USER cp /vagrant/configs/provision/server.d/update_code.sh /home/$INSTALL_USER
+sudo -u $INSTALL_USER cp /vagrant/scripts/start_jackd.sh /home/$INSTALL_USER
+chmod +x /home/$INSTALL_USER/{update_code.sh,start_jackd.sh}
+
+rsync -rv /vagrant/{scripts,src} /home/$INSTALL_USER/territorios/
 /etc/rc.local
-sleep 3
-sudo -u $INSTALL_USER /home/$INSTALL_USER/update_code.sh
 
