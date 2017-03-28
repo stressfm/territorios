3082fc9d96be0d4631e9f56b316569e315c17952
vasco <vascopitaf@gmail.com>
2017-03-28 11:06:54 +0100

    added stop command for emitter

--- src/client.py
+++ src/client.py
@@ -228,6 +228,7 @@ class Matriz:
             Receiver.receivers[receiver].stop()
         if self.emitter is not None:
             logging.debug("Not shutting down emitter.")
+            self.emitter.stop()
 
     def get_port(self):
         logging.debug('Discovering... delay={}ums'.format(self.upnp_client.discoverdelay))
